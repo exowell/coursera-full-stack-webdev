ac650399b60470912a08c32187946e5240b9a131
Gabor Szoboszlai <szobinet@gmail.com>
2017-07-25 19:46:41 +0200

    ExpressJS routers assignment

--- NodeServer/server.js
+++ NodeServer/server.js
@@ -1,16 +1,22 @@
-
 var express = require('express');
 var morgan = require('morgan');
+var dishRouter = require('./router/dishRouter');
+var promoRouter = require('./router/promoRouter');
+var leaderRouter = require('./router/leaderRouter');
 
 var hostname = 'localhost';
-var port = 3100;
+var port = 3000;
 
 var app = express();
 
 app.use(morgan('dev'));
 
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leadership', leaderRouter);
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, hostname, function(){
+app.listen(port, hostname, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
